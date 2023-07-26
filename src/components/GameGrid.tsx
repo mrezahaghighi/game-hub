8fd1ae91b4ef7c97e9ea68136aff4748a429c64f
mreza <haghighi.mreza@gmail.com>
2023-07-27 01:31:22 +0330

    displaying platform icons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,5 +1,6 @@
-import { Text } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
+import GamesCard from "./GamesCard";
 
 const GameGrid = () => {
   const { games, error } = useGames();
@@ -7,11 +8,11 @@ const GameGrid = () => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <ul>
+      <SimpleGrid spacing={10} columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}>
         {games.map((game) => (
-          <li key={game.id}>{game.name}</li>
+          <GamesCard key={game.id} game={game} />
         ))}
-      </ul>
+      </SimpleGrid>
     </>
   );
 };
